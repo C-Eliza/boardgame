0e14ad0e0f7240193d5bbf4fd162cf99fce48276
Eliza Canales <elizacanales777@icloud.com>
2024-10-06 14:37:51 -0600

    Added random creation and color representation for numbers

--- src/game.c
+++ src/game.c
@@ -11,8 +11,52 @@ typedef enum{
     PLAYER_LOSE=-1,
     RUNNING_STATE = 2} GameState;
 
-void drawGrid();
+//initializes the game and seed, hopefully prints grid too.
+int **init_game(int size)
+{
+    srand(time(NULL));
+    
+    //Creating dynamically sized array
+    int **valueGrid;
+    valueGrid = malloc(size * sizeof(*valueGrid));
+    for(int i = 0; i < size; i++) valueGrid[i] = malloc(size * sizeof(*valueGrid[i]));
+    for(int i = 0; i < size; i++) for(int j = 0; j < size; j++) valueGrid[i][j] = rand() % 6;
+
+    return valueGrid;
+}
+ 
+void printColorChunk(int color){
+  //prints out the four lines of color given a number
+  switch(color)
+  {
+    case 0: printf(REDBG hfsqr  RESET);
+      break;
+    case 1: printf(GREENBG hfsqr  RESET);
+      break;
+    case 2: printf(YELLOWBG hfsqr  RESET);
+      break;
+    case 3: printf(BLUEBG hfsqr  RESET);
+      break;
+    case 4: printf(MAGENTABG hfsqr  RESET);
+      break;
+    case 5: printf(CYANBG hfsqr  RESET);
+      break;
+  }
+}
+
+void drawGrid(int **valueGrid, int size){
+  //printing different colors for each possible grid value
+  for(int i = 0; i < size; i++)
+    for(int k = 0; k < 3; k++){
+      for(int j = 0; j < size; j++)
+        //printColorChunk(valueGrid[i][j]);
+        printf(&COLORS[valueGrid[i][j]] hfsqr  RESET);
+      printf("\n");
+    };
+}
+
 int main(){
+  /*
     //initial state is dead
     GameState gameState=0;
     printf(BLUEBG hfsqr  RESET);
@@ -23,16 +67,12 @@ int main(){
     printf(REDBG hfsqr   RESET);
     printf(GREENBG hfsqr RESET"\n");
     return 0;
-}
+  */
+  int size = 7;
+  int **valueGrid = init_game(size);
+  drawGrid(valueGrid, size);
 
-//initializes the game and seed, hopefully prints grid too.
-int init_game()
-{
-    srand(time(NULL));
-    drawGrid();
-    return 0;
 }
-
 
 void updateState(GameState *state)
 {
